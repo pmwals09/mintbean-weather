8f7f69b835be0de0d74f863793791c15b469ffad
Pat Walsh <pmwals09@gmail.com>
2020-06-24 16:17:27 -0400

    More data, more cleaning

--- src/components/SingleTile.js
+++ src/components/SingleTile.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import _ from 'lodash'
 
 const SingleTile = ({weather, duration}) => {
   const cardDate = new Date(weather.dt * 1000).toString()
   const formattedCardDate = duration === 'daily' ? cardDate.slice(0,10) : cardDate.slice(0,10).concat(cardDate.slice(15,21))
 
-  const temps = weather.temp.day ? {a: weather.temp.max, b: weather.temp.min} : {a: weather.temp, b: weather.feels_like}
+  const temps = weather.temp.day ? {'Max:': weather.temp.max, 'Min:': weather.temp.min} : {'Temp:': weather.temp, 'Feels like:': weather.feels_like}
+  const formattedTemps = _.zip(Object.keys(temps), Object.values(temps)).map(ea => ea.join(' ')).join(' / ')
+
+  const sunrise = new Date(weather.sunrise * 1000).toString().slice(15,21)
+  const sunset = new Date(weather.sunset * 1000).toString().slice(15,21)
+
   return (
     <div className='cell small-3 text-center'>
       <div className='card'>
@@ -15,8 +21,10 @@ const SingleTile = ({weather, duration}) => {
           <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
         </div>
         <div className='card-section'>
-          <p>Temp: {temps.a} / Feels like: {temps.b}</p>
-          <p>{weather.weather[0].main}: {weather.weather[0].description}</p>
+          <p>{formattedTemps}</p>
+          <p>Humidity: {weather.humidity}%</p>
+          <p>{_.capitalize(weather.weather[0].description)}</p>
+          {weather.sunrise && <p>Sunrise: {sunrise} / Sunset: {sunset}</p>}
         </div>
       </div>
     </div>
