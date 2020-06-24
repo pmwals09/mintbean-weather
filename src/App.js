10a8df0f324b120db9ff1371bf9e402d293e8913
Pat Walsh <pmwals09@gmail.com>
2020-06-24 12:53:10 -0400

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <h1 className='text-center'>Check your local weather</h1>
+      <div className='grid-y'>
+        <div className="cell">
+          <div className='grid-x align-center'>
+            <div className='small-4'>
+              <form>
+                <div className='input-group'>
+                  <label htmlFor="zip" className='input-group-label'>Zip</label>
+                  <input type="text" id="zip" name="zip" className='input-group-field'/>
+                  <div className='input-group-button'>
+                    <input type="submit" value="Submit" className="button"/>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className='cell'>
+          <h3 className='text-center'>OR</h3>
+        </div>
+        <div className="cell">
+          <div className='grid-x align-center'>
+            <div className='small-4'>
+              <form>
+                <div className='input-group'>
+                  <label htmlFor="city" className='input-group-label'>City</label>
+                  <input type="text" id="city" name="city" className='input-group-field'/>
+                  <div className='input-group-button'>
+                    <input type="submit" value="Submit" className="button"/>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
