71a4d66fffd50068392d3ad7d0121b9bd3249f40
Pat Walsh <pmwals09@gmail.com>
2020-06-24 13:46:03 -0400

    Form working, new API w Google means simplified hierarchy

---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const Form = ({field, label, fetchLocation}) => {
+  const defaultState = {
+    fieldVal: ''
+  }
+  const [formValues, setFormValues] = useState(defaultState)
+
+  const handleChange = event => {
+    setFormValues({
+      ...formValues,
+      fieldVal: event.currentTarget.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    fetchLocation(formValues)
+  }
+
+  return (
+    <div className='grid-x align-center'>
+      <div className='small-4'>
+        <form onSubmit={handleSubmit}>
+          <div className='input-group'>
+            <label htmlFor={`${field}`} className='input-group-label'>
+              {label}
+            </label>
+            <input
+              type="text"
+              id={`${field}`}
+              name={`${field}`}
+              className='input-group-field'
+              value={formValues.fieldVal}
+              onChange={handleChange}
+            />
+            <div className='input-group-button'>
+              <input type="submit" value="Submit" className="button"/>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Form
